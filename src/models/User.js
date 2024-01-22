28a28cb7120395fbda0961dbd19323cc0f99bc0a
Luis Sousa <las.sousa@outlook.com>
2024-01-22 15:46:35 -0300

    User schema

---
+++ src/models/User.js
@@ -0,0 +1,45 @@
+// {
+//     "name": "Luis Sousa",
+//     "username": "luisinho",
+//     "email": "luisinho@luisinho.com",
+//     "password": "senha",
+//     "avatar": "https://avatars.githubusercontent.com/u/70407063?v=4",
+//     "background":"https://i.imgur.com/ZRUSVcp.jpg"
+//   }
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String,
+            required: true,
+        },
+        background: {
+            type: String,
+            required: true,
+        },
+    }
+)
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User
