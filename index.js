28308ce95da92d347e05e3eb9d06231477175a3a
Luis Sousa <las.sousa@outlook.com>
2024-01-22 18:32:16 -0300

    reorganizing index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const app = express()
-app.use(express.json());
-
+const connectDatabase = require('./src/database/db')
 const userRoute = require('./src/routes/user-route')
+
+const app = express()
+const port = 3000
+
+connectDatabase()
+app.use(express.json());
 app.use('/user', userRoute)
 
-const connectDatabase = require('./src/database/db')
-connectDatabase()
-
-
-const port = 3000
 app.listen(port, () => console.log('Servidor online na porta', port))
