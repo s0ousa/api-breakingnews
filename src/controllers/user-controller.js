08352e2fb379d9cd8c4764ae25713f4bd135a58a
Luis Sousa <las.sousa@outlook.com>
2024-01-22 18:24:21 -0300

    add user-service

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,15 +1,29 @@
-const create = (req, res) => {
-    const user = req.body
-    const { name, username, email, password, avatar, background } = user
+const userService = require('../services/user-service')
+
+const create = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body
 
     if (!name || !username || !email || !password || !avatar || !background) {
         res.status(400)
             .send({ message: "Submit all fields for registration, please." })
     }
 
+    const user = await userService.create(req.body)
+    if (!user) {
+        res.status(400)
+            .send({ message: "Error creating User" })
+    }
+
     res.status(201).send({
         message: "User created successfully",
-        user
+        user: {
+            id: user._id,
+            name,
+            username,
+            email,
+            avatar,
+            background
+        }
     })
 }
 
