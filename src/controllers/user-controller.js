955f72fdac95f91bd1ac2fa3a70b67d0cb923bd7
Luis Sousa <las.sousa@outlook.com>
2024-01-22 21:36:09 -0300

    add user find all + user find by id

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,4 +1,5 @@
 const userService = require('../services/user-service')
+const mongoose = require('mongoose')
 
 const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body
@@ -27,4 +28,29 @@ const create = async (req, res) => {
     })
 }
 
-module.exports = { create }
+const findAll = async (req, res) => {
+    const users = await userService.findAll()
+
+    if (users.length === 0) {
+        return res.status(400).send({ message: 'Não há usuários cadastrados ou houve um erro.' })
+    }
+
+    res.send(users)
+}
+
+const findById = async (req, res) => {
+    const id = req.params.id
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Id invalido" })
+    }
+
+    const user = await userService.findById(id)
+    if (!user) {
+        return res.status(400).send({ message: "Usuario nao encontrado" })
+    }
+
+    res.send(user)
+}
+
+module.exports = { create, findAll, findById }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,6 +2,12 @@ const User = require('../models/User')
 
 const create = body => User.create(body)
 
+const findAll = () => User.find()
+
+const findById = (id) => User.findById(id)
+
 module.exports = {
     create,
+    findAll,
+    findById
 }
