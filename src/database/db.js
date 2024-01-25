f278f7ba4d33c74a6cbde6190afdf9cdc01d6fb6
Luis Sousa <las.sousa@outlook.com>
2024-01-25 10:35:19 -0300

    add .env

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
 
 const connectDatabase = () => {
     console.log('Conectando ao banco de dados...')
     mongoose.connect(
-        'mongodb+srv://root:r2NNDh0DEvnPsyWR@cluster0.otnbiyf.mongodb.net/?retryWrites=true&w=majority')
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.otnbiyf.mongodb.net/?retryWrites=true&w=majority`)
         .then(() => console.log('Conexão estabelecida com MongoDB Atlas'))
         .catch(erro => console.log(erro))
 }
