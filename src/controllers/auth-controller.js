6c1db5e7c87897dffc9d13a1069f0a46faeab862
Luis Sousa <las.sousa@outlook.com>
2024-01-30 18:36:08 -0300

    adding jwt

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import loginService from '../services/auth-service.js';
+import { loginService, generateToken } from '../services/auth-service.js';
 
 
 const login = async (req, res) => {
@@ -7,7 +7,6 @@ const login = async (req, res) => {
 
     try {
         const user = await loginService(email)
-
         if (!user) {
             return res.status(404).send({ message: 'Invalid username or password' })
         }
@@ -17,7 +16,8 @@ const login = async (req, res) => {
             return res.status(404).send({ message: 'Invalid username or password' })
         }
 
-        res.send(user)
+        const token = generateToken(user.id)
+        res.send({ token })
     } catch (error) {
         res.status(500).send(error.message)
     }
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,5 +1,11 @@
 import { User } from '../models/User.js'
+import jwt from 'jsonwebtoken'
 
-const login = (email) => User.findOne({ email: email }).select('+password')
+const loginService = (email) => User.findOne({ email: email }).select('+password')
 
-export default login
+const generateToken = (id) => jwt.sign(
+    { id: id },
+    process.env.SECRET_JWT,
+    { expiresIn: 86400 })
+
+export { loginService, generateToken }
