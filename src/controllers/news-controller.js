a7edf5c40145b066ff207153f7829788991ea58c
Luis Sousa <las.sousa@outlook.com>
2024-02-13 16:51:59 -0300

    news pagination

--- src/controllers/news-controller.js
+++ src/controllers/news-controller.js
@@ -26,12 +26,48 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
     try {
-        const news = await newsService.findAll()
+        let { limit, offset } = req.query
+        limit = Number(limit)
+        offset = Number(offset)
 
+        if (!limit) {
+            limit = 5
+        }
+
+        if (!offset) {
+            offset = 0
+        }
+
+        const news = await newsService.findAll(offset, limit)
+        const total = await newsService.countNews()
+        const currentUrl = req.baseUrl
+
+        const next = offset + limit
+        const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null
+
+        const previous = offset - limit < 0 ? null : offset - limit
+        const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null
         if (news.length === 0) {
             res.status(400).send({ message: "There are no registered news" })
         }
-        res.send(news)
+        res.send({
+            nextUrl,
+            previousUrl,
+            limit,
+            offset,
+            total,
+            results: news.map(item => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                coments: item.coments,
+                name: item.user.name,
+                username: item.user.username,
+                userAvatar: item.user.avatar
+            }))
+        })
     } catch (error) {
         res.status(500).send({ message: error.message })
     }
--- src/services/news-service.js
+++ src/services/news-service.js
@@ -2,10 +2,17 @@ import News from '../models/News.js'
 
 const create = (body) => News.create(body)
 
-const findAll = () => News.find()
+const findAll = (offset, limit) =>
+    News.find()
+        .sort({ _id: -1 })
+        .skip(offset)
+        .limit(limit)
+        .populate("user")
 
+const countNews = () => News.countDocuments()
 
 export default {
     create,
-    findAll
+    findAll,
+    countNews
 }
