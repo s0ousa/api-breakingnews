f297bb113f3b38b4800278b4e42a48f3fde2e301
Luis Sousa <las.sousa@outlook.com>
2024-02-14 18:32:10 -0300

    find by id route GET

--- src/controllers/news-controller.js
+++ src/controllers/news-controller.js
@@ -74,6 +74,29 @@ const findAll = async (req, res) => {
 
 }
 
+const findById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const news = await newsService.findById(id)
+
+        return res.send({
+            news: {
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                banner: news.banner,
+                likes: news.likes,
+                coments: news.coments,
+                name: news.user.name,
+                username: news.user.username,
+                userAvatar: news.user.avatar
+            }
+        })
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
 const topNews = async (req, res) => {
 
     try {
@@ -102,4 +125,4 @@ const topNews = async (req, res) => {
 
 }
 
-export { create, findAll, topNews }
+export { create, findAll, topNews, findById }
--- src/services/news-service.js
+++ src/services/news-service.js
@@ -2,6 +2,12 @@ import News from '../models/News.js'
 
 const create = (body) => News.create(body)
 
+const findById = (id) => News.findById(id).populate("user")
+
+const countNews = () => News.countDocuments()
+
+const topNews = () => News.findOne().sort({ _id: -1 }).populate("user")
+
 const findAll = (offset, limit) =>
     News.find()
         .sort({ _id: -1 })
@@ -9,13 +15,10 @@ const findAll = (offset, limit) =>
         .limit(limit)
         .populate("user")
 
-const countNews = () => News.countDocuments()
-
-const topNews = () => News.findOne().sort({ _id: -1 }).populate("user")
-
 export default {
     create,
     findAll,
     countNews,
-    topNews
+    topNews,
+    findById
 }
