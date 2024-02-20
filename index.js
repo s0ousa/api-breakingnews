92ac345fff1046027c6ca93e725e7352656ccd50
Luis Sousa <las.sousa@outlook.com>
2024-02-20 17:16:35 -0300

    documentation with swagger

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDatabase from './src/database/db.js'
 import userRoute from './src/routes/user-route.js'
 import authRoute from './src/routes/auth-route.js'
 import newsRoute from './src/routes/news-route.js'
+import swaggerRoute from './src/routes/swagger-route.js'
 
 const app = express()
 const port = process.env.port || 3000
@@ -13,5 +14,6 @@ app.use(express.json());
 app.use('/user', userRoute)
 app.use('/auth', authRoute)
 app.use('/news', newsRoute)
+app.use('/doc', swaggerRoute)
 
 app.listen(port, () => console.log('Servidor online na porta', port))
